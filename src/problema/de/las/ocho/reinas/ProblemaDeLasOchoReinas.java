4dd55e7665cb86b22ac5fc3a844fed40332a8448
wnbuitragoc <wnbuitragoc@correo.udistrital.edu.co>
2020-06-04 22:26:36 -0500

    Algoritmo en Java

---
+++ src/problema/de/las/ocho/reinas/ProblemaDeLasOchoReinas.java
@@ -0,0 +1,13 @@
+
+package problema.de.las.ocho.reinas;
+
+
+public class ProblemaDeLasOchoReinas {
+
+  
+    public static void main(String[] args) {
+        Marco miMarco=new Marco();
+        miMarco.setVisible(true);
+    }
+    
+}
